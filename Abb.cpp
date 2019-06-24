0bf5df8e94ecf0cde57387652002256849a1e224
jmalvarez97 <45094891+jmalvarez97@users.noreply.github.com>
2019-06-24 14:38:02 -0300

    Add files via upload

--- Abb.cpp
+++ Abb.cpp
@@ -3,30 +3,69 @@
 
 Abb::Abb(){
 	raiz = new Nodo_abb;
+	cout << "Pido Memoria Arbol" << endl;
 	raiz = NULL;
-	altura = 0;
 }
 
-Abb::Abb(Nodo_abb* nueva_raiz,int nueva_altura){
+Abb::Abb(Nodo_abb* nueva_raiz){
 	raiz = new Nodo_abb;
-	altura = new int;
 	raiz = nueva_raiz;
-	*altura = nueva_altura;
 }
 
 Nodo_abb* Abb::obtener_raiz(){
 	return raiz;
 }
 
-int Abb::obtener_altura(){
-	return *altura;
-}
-
 void Abb::asignar_raiz(Nodo_abb* nueva_raiz){
 	raiz = nueva_raiz;
 }
 
+bool Abb::es_vacio(){
+	return raiz == NULL;
+}
+
+void Abb::agregar(Nodo_abb* nodo_a_agregar){
+	bool fue_agregado = false;
+
+	if(es_vacio()){
+		asignar_raiz(nodo_a_agregar);
+		fue_agregado = true;
+	}
+
+	Nodo_abb* actual = obtener_raiz();
+	
+	while(!fue_agregado){
+		if(nodo_a_agregar -> obtener_telefono() >= actual->obtener_telefono()){
+			if(actual->obtener_hijo_der() == NULL){
+				actual -> asignar_hijo_der(nodo_a_agregar);
+				fue_agregado = true;
+			}
+			else{
+				actual = actual -> obtener_hijo_der();
+			}
+		}
+		else{
+			if(actual->obtener_hijo_izq() == NULL){
+				actual->asignar_hijo_izq(nodo_a_agregar);
+				fue_agregado = true;
+			}
+			else{
+				actual = actual -> obtener_hijo_izq();
+			}
+		}
+	}
+}
+
+void Abb::borrar(Nodo_abb* pos){
+	if(!pos) return;
+	borrar(raiz -> obtener_hijo_izq());
+	borrar(raiz -> obtener_hijo_der());
+	delete pos;
+}
+
+
 Abb::~Abb(){
-	delete raiz;
-	delete altura;
+	borrar(raiz);
+	//delete raiz;
+	cout << "Borro raiz";
 }
--- Cliente.h
+++ Cliente.h
@@ -1,22 +1,25 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
+#include <iostream>
+using namespace std;
+const float descuento_familia =  0.65;
+const float descuento_individuo = 0.9;
+
 class Cliente{
 
 private:
 	bool alta;
-	int* legajo;
+	string* legajo;
+	int precio_base;
+	char tipo;
 
 public:
- 	//Descripcion:Constructor sin parametros
-	//Pre:---
-	//Post:Crea un objeto cliente
- 	Cliente();                      ////// es al pedo crearlo sin parametros (?)
-
+	Cliente();
  	//Descripcion:Constructor con parametros
 	//Pre:---
 	//Post:Crea un objeto cliente
- 	Cliente(bool nueva_alta, int nuevo_legajo);
+ 	Cliente(bool nueva_alta, string nuevo_legajo, int nuevo_precio_base, char nuevo_tipo);
 
  	//Pre:El objeto debe existir
 	//Post:Devuelve si esta de alta o no
@@ -24,7 +27,15 @@ public:
 
  	//Pre:El objeto debe existir
 	//Post:Devuelve el numero de legajo
-	int obtener_legajo();
+	string obtener_legajo();
+
+	//Pre:El objeto debe existir
+	//Post:Devuelve el precio base
+	int obtener_precio();
+
+	//Pre:El objeto debe existir
+	//Post:Devuelve el tipo cliente siendo F familia e I individuo
+	char obtener_tipo();
 
 	//Pre:El objeto debe existir
 	//Post:Se modifica el estado de alta
@@ -32,7 +43,18 @@ public:
 
 	//Pre:El objeto debe existir
 	//Post:Se modifica el numero de legajo
-	void asignar_legajo(int nuevo_legajo);
+	void asignar_legajo(string nuevo_legajo);
+
+	int obtener_precio_base();
+
+	virtual int obtener_tamanio_cliente();
+
+	virtual void mostrar_cliente();
+
+	//Descripcion:Destructor
+	//Pre:El objeto debe existir
+	//Post:El objeto es destruido
+	virtual ~Cliente();
 
 };	
 
