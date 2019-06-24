e162f27eeea062ad7e5b9c04edbf6eb070cca283
Martinsosa95 <49997897+Martinsosa95@users.noreply.github.com>
2019-06-24 17:08:29 -0300

    Add files via upload

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,23 +1,53 @@
 #include "Cliente.h"
 
-Cliente::Cliente(bool nueva_alta, int nuevo_legajo){
-	legajo = new int;
+Cliente::Cliente(){
+	legajo = new string;
+	alta = true;
+	//legajo = "";
+	precio_base = 0;
+	tipo = 'N';
+}
+
+Cliente::Cliente(bool nueva_alta, string nuevo_legajo, int nuevo_precio_base, char nuevo_tipo){
+	legajo = new string;
+	cout << "Pido Memoria cliente" << endl;
 	alta = nueva_alta;
 	*legajo = nuevo_legajo;
+	precio_base = nuevo_precio_base;
+	tipo = nuevo_tipo;
 }
 
 bool Cliente::obtener_alta(){
 	return alta;
 }
 
-int Cliente::obtener_legajo(){
+string Cliente::obtener_legajo(){
 	return *legajo;
+}
+
+int Cliente::obtener_precio_base(){
+	return precio_base;
+}
+
+char Cliente::obtener_tipo(){
+	return tipo;
 }
 
 void Cliente::asignar_alta(bool nueva_alta){
 	alta = nueva_alta;
 }
 
-void Cliente::asignar_legajo(int nuevo_legajo){
+void Cliente::asignar_legajo(string nuevo_legajo){
 	*legajo = nuevo_legajo;
+}
+
+int Cliente::obtener_tamanio_cliente(){
+	return 0;
+}
+
+void Cliente::mostrar_cliente(){}
+
+Cliente::~Cliente(){
+	delete legajo;
+	cout << "Borro legajo" << endl;
 }
--- Funciones_main.h
+++ Funciones_main.h
@@ -1,1 +1,28 @@
-Funciones_main.h
+#ifndef FUNCIONES_MAIN_H
+#define FUNCIONES_MAIN_H
+
+#include "Lista.h"
+#include "Nodo_lista.h"
+#include "Abb.h"
+#include "Nodo_abb.h"
+#include "Cliente.h"
+#include "Familia.h"
+#include "Individuo.h"
+#include <fstream>
+#include <string>
+
+void menu();
+int cantidad_espacios(string lectura);
+void cargar_clientes(string lectura, Lista<string>* clientes);
+void cargar_abb(ifstream& archivo,Abb* arbol,int);
+Nodo_abb* buscar_telefono(string telefono, Abb* arbol);
+void dar_de_baja(string telefono, Abb* arbol);
+float precio_producto(string telefono, Abb* arbol);
+void listar_abb(Abb* arbol,Nodo_abb* raiz);
+string pedir_telefono();
+string* pedir_nombre();
+int pedir_legajo();
+void agregar_cliente(int precio_base,Abb* arbol);
+
+
+#endif // FUNCIONES_MAIN_H
