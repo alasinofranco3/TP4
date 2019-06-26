288789d75882e67f5147ba752cc0b50c68aabbce
jmalvarez97 <45094891+jmalvarez97@users.noreply.github.com>
2019-06-26 11:06:46 -0300

    Add files via upload

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,23 +1,51 @@
 #include "Cliente.h"
 
-Cliente::Cliente(bool nueva_alta, int nuevo_legajo){
-	legajo = new int;
+Cliente::Cliente(){
+	legajo = new string;
+	alta = true;
+	precio_base = 0;
+	tipo = 'n';
+}
+
+Cliente::Cliente(bool nueva_alta, string nuevo_legajo, int nuevo_precio_base, char nuevo_tipo){
+	legajo = new string;
+	precio_base = new int
 	alta = nueva_alta;
 	*legajo = nuevo_legajo;
+	precio_base = nuevo_precio_base;
+	tipo = nuevo_tipo;
 }
 
 bool Cliente::obtener_alta(){
 	return alta;
 }
 
-int Cliente::obtener_legajo(){
+string Cliente::obtener_legajo(){
 	return *legajo;
+}
+
+int Cliente::obtener_precio_base(){
+	return precio_base;
+}
+
+char Cliente::obtener_tipo(){
+	return tipo;
 }
 
 void Cliente::asignar_alta(bool nueva_alta){
 	alta = nueva_alta;
 }
 
-void Cliente::asignar_legajo(int nuevo_legajo){
+void Cliente::asignar_legajo(string nuevo_legajo){
 	*legajo = nuevo_legajo;
+}
+
+int Cliente::obtener_tamanio_cliente(){
+	return 0;
+}
+
+void Cliente::mostrar_cliente(){}
+
+Cliente::~Cliente(){
+	delete legajo;
 }
--- Individuo.cpp
+++ Individuo.cpp
@@ -1,14 +1,25 @@
 #include "Individuo.h"
 
-Individuo::Individuo(string nuevo_nombre,bool nueva_alta,int nuevo_telefono) : Cliente(nueva_alta,nuevo_telefono){
-	nombre = new string;
-	*nombre = nuevo_nombre;
+Individuo::Individuo(string nuevo_nombre,bool nueva_alta,string nuevo_legajo, int nuevo_precio_base, char nuevo_tipo) : Cliente(nueva_alta,nuevo_legajo, nuevo_precio_base, nuevo_tipo){
+	nombre = new string(nuevo_nombre);
 }
 
 string Individuo::obtener_individuo(){
 	return *nombre;
 }
 
-void Individuo::asignar_individuo(string nuevo_nombre){
-	*nombre = nuevo_nombre;
+void Individuo::asignar_individuo(string* nuevo_nombre){
+	nombre = nuevo_nombre;
+}
+
+int Individuo::obtener_tamanio_cliente(){
+	return 1;
+}
+
+void Individuo::mostrar_cliente(){
+	cout << "Miembro 1: " << *nombre << endl; 
+}
+
+Individuo::~Individuo(){
+	delete nombre;
 }
